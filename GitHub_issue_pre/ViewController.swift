5eac64f9ff7fc3bc2397c4ff2d660b4f669c6ce0
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-09-03 17:49:57 +0900

    クエリを指定した時のJSONデータに対するパースは成功

--- GitHub_issue_pre/ViewController.swift
+++ GitHub_issue_pre/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         
         let issues_run = Issue_number()
         issues_run.issue()
+        //issues_run.issues_List()
         
         
         //play.issue()
@@ -27,111 +28,10 @@ class ViewController: UIViewController {
     }
 }
 
-//GitHub上で作成したトークンを用いて認証を行った
-//自分のアカウントへのアクセスURL:取得できているのでOK
-//let urlString :String = "https://api.github.com/repos/ookamiinc/ios/issues/ access_token=0ce6965a6ad1bdf7f547f51d6e66a85529f7a711"
-
-//お手本のurl：このurlからはデータを取得できているのでOK
-//let urlString: String="https://api.github.com/search/issues?q=windows+label:bug+language:python+state:open&sort=created&order=asc"
-
-
-
-//ookamiincへのアクセスが通らない
-//usernameとpasswordが必要！
-//let urlString:String = "https://api.github.com/repos/ookamiinc/ios/issues/"
-//let parameter : String = "q"
-
-/*
- func getissue(){
- Alamofire.request(urlString, method:.get)
- .responseJSON{ Response in
- print(Response.result.value)
- }
- }
- 
- func view_issue(){
- getissue()
- }
- */
-
-
-
-
-
-
-/*
- class Issues{
- 
- func issue(){
- Alamofire.request( urlString,method:.get).responseJSON {
- response in
- switch response.result {
- case .success(let Value):
- print(Value)
- value_issue = Value
- case .failure(let Error):
- print(Error)
- }
- }
- }
- struct Issues_JSON : Codable {
- var body:String
- }
- 
- let decoder : JSONDecoder = JSONDecoder()
- func Encoder(){
- do {
- let json : Issues_JSON = try decoder.decode(Issues_JSON.self,from:value_issue as! Data)
- print(json)
- }
- catch{
- print("error",error.localizedDescription)
- }
- }
- 
- 
- }
- */
-
-//basic認証をtryするために作ったコード
-/*
- func issue(){
- Alamofire.request( ookami_url, method : .post )
- .authenticate(user: user, password: password)
- .response { response in
- print(response)
- 
- }
- }
- */
-
-/*
- let credentialData = ("\(user):\(password)").data(using : String.Encoding.utf8)!
- let base64Credentials = credentialData.base64EncodedString(options : [])
- var request = URLRequest(url: NSURL(string : ookami_url)! as URL )
- var headers: HTTPHeaders = [:]
- 
- func issue(){
- let credentialData = ("\(user):\(password)").data(using: String.Encoding.utf8)
- let base64Credentials = credentialData!.base64EncodedString(options : [])
- 
- if let authorizationHeader = Request.authorizationHeader(user: user, password: password) {
- headers[authorizationHeader.key] = authorizationHeader.value
- 
- Alamofire.request(ookami_url, headers: headers)
- .validate().responseJSON { response in
- switch response.result{
- 
- case .success(let Value):
- print(Value)
- 
- case .failure:
- print("error")
- }
- }
- }
- }
- */
+//配列型のパースを実施するためのプロトコルを定義
+public protocol Decodable {
+    init(from decoder: Decoder) throws
+}
 
 //basicm認証 成功! 2019/08/31
 //アクセス方法に自身で設定したアクセストークンを設定したことで
@@ -141,8 +41,15 @@ let user = "rei-yoshi"
 let password = "rei07041998"
 var value_issue:Any = 0
 var query_number : Int = 0
+var issue_number = 1
+let httpHeaderRequests = ["Authorization" : "b6ff772c33cf01489563c06a3d051d340e881c77","Accept" : "application/vnd.github.v3.text+json"]
 
-let ookami_issues_url:String = "https://api.github.com/repos/ookamiinc/ios/issues/4907"
+//クエリを指定
+let ookami_issues_url:String = "https://api.github.com/repos/ookamiinc/ios/issues/\(issue_number)"
+
+
+let ookami_issues_List_url : String = "https://api.github.com/repos/ookamiinc/ios/issues"
+
 let ookami_issues_comments_url = "https://api.github.com/repos/ookamiinc/ios/issues/4862/comments"
 let password1 = "b6ff772c33cf01489563c06a3d051d340e881c77"
 
@@ -160,18 +67,77 @@ var parameters : Parameters? {
     ]
 }
 
-//issueの最新のnumbeρを取得する
+
+
+//issueの最新のnumberを取得する
 class Issue_number {
     
-    var parameters :Parameters = [String : Any] ()
-    //put slash before \ (base64Credentials)
     
-    //number取得メソッド
+    
+    /*
+     //issuesのそれぞれのtitleを抽出して一覧表示しようと考えたが
+     //({title="",body="",...},{title="",body="",..},{title="",body="",...}のような形式の
+     //JSONデータのパースを実行できていないため一旦保留
+     
+     
+    
+    //issues一覧のtitleを取得するメソッド
+    //ルートが配列の時のパースを実装する
+    func issues_List(){
+        Alamofire.request(ookami_issues_List_url, method: .get, parameters: parameters,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
+            
+            /*
+            switch response.result {
+            case .success(let value):
+                print(value)
+                value_issue = value
+            //your success code
+            case .failure(_):
+                print("error")
+            }
+ */
+            guard let data=response.data else {
+                return
+            }
+            let decorder = JSONDecoder()
+            
+            
+            //guard文で書き変え
+            //issuesのtitleを取得する用
+            
+            struct Issues_List : Codable {
+                var title : String
+                var body: String
+                var number : Int
+                var comments_url : String
+            }
+
+            do{
+                let issue_List: Issues_List = try decorder.decode(Issues_List.self,from: data)
+                //issues_JSON型で
+                //print(task)
+                
+                print(issue_List.title)
+                print(issue_List.body)
+                
+                
+                query_number = issue_List.number
+            }
+            catch{
+                print(error)
+            }
+        }
+    }
+    
+    */
+    
+    
+    //特定のクエリを指定した時のパースを実装したもの
     func issue(){
         Alamofire.request(ookami_issues_url, method: .get, parameters: parameters,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
             
             
-            /*
+            
              switch response.result {
              case .success(let value):
              print(value)
@@ -180,19 +146,21 @@ class Issue_number {
              case .failure(_):
              print("error")
              }
- */
+             
             
             
             //guard文で書き変え
             //パースすることに成功
+            //queryを指定した時に実施する用
             //numberの取得
-            
             struct issues_JSON : Codable {
                 var title : String
                 var body: String
                 var number : Int
                 var comments_url : String
             }
+            
+            
             
             guard let data=response.data else {
                 return
@@ -205,38 +173,22 @@ class Issue_number {
                 print(task.title)
                 print(task.number)
                 print(task.body)
-                print(task.comments_url)
+            
+                print("クエリ設定時parseは成功!!")
+                
+                for _ in 1...7{
+                    print("")
+                }
+                
                 query_number = task.number
                 
             }
             catch{
                 print(error)
             }
-            
-            
-            
-            
         }
     }
-    
-    
 }
-
-
-
-
-
-
-
-//2019/09/02
-//JSON形式のパースの実施
-
-
-
-
-
-
-
 
 
 
