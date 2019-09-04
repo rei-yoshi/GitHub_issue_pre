483eff61eff1a364ef9153a127ebf42ffaa2f713
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-09-04 14:36:18 +0900

    UILabel の追加

--- GitHub_issue_pre/ViewController.swift
+++ GitHub_issue_pre/ViewController.swift
@@ -7,9 +7,6 @@
 //
 
 import UIKit
-import Alamofire
-import Foundation
-
 
 
 class ViewController: UIViewController {
@@ -17,180 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var IssueNumber_specification: UITextField!
     @IBOutlet weak var searchIssue_Button: UIButton!
     
+    var issue_number:Int = 0
+    
+    //textFieldの数値を保存用
+    var textField_Number = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        
-        let issues_run = Issue_number()
-        issues_run.issue()
-        //issues_run.issues_List()
-    
-       
-    }
-}
-
-
-
-
-//配列型のパースを実施するためのプロトコルを定義
-public protocol Decodable {
-    init(from decoder: Decoder) throws
-}
-
-//basicm認証 成功! 2019/08/31
-//アクセス方法に自身で設定したアクセストークンを設定したことで
-//ookami_organizationへのアクセス権限を得ることができた
-
-let user = "rei-yoshi"
-let password = "rei07041998"
-var value_issue:Any = 0
-var query_number : Int = 0
-var issue_number = 4909
-
-
-//クエリを指定
-let ookami_issues_url:String = "https://api.github.com/repos/ookamiinc/ios/issues/\(issue_number)"
-
-
-//let ookami_issues_List_url : String = "https://api.github.com/repos/ookamiinc/ios/issues"
-
-//let ookami_issues_comments_url = "https://api.github.com/repos/ookamiinc/ios/issues/4862"
-
-
-let password1 = "93aed37553e3037ea220d3f1fbd34c2e75fb4fc1"
-let credentialData = "\(user):\(password1)".data(using: String.Encoding.utf8)!
-let base64Credentials = credentialData.base64EncodedString()
-let headers = [
-    "Authorization": "Basic \(base64Credentials)",
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-]
-
-
-
-
-//issueの最新のnumberを取得する
-class Issue_number {
-    
-    
-    
-    /*
-     //issuesのそれぞれのtitleを抽出して一覧表示しようと考えたが
-     //({title="",body="",...},{title="",body="",..},{title="",body="",...}のような形式の
-     //JSONデータのパースを実行できていないため一旦保留
-     //余裕があればやる
-     
-     
-    
-    //issues一覧のtitleを取得するメソッド
-    //ルートが配列の時のパースを実装する
-    func issues_List(){
-        Alamofire.request(ookami_issues_List_url, method: .get, parameters: parameters,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
-            
-            /*
-            switch response.result {
-            case .success(let value):
-                print(value)
-                value_issue = value
-            //your success code
-            case .failure(_):
-                print("error")
-            }
- */
-            guard let data=response.data else {
-                return
-            }
-            let decorder = JSONDecoder()
-            
-            
-            //guard文で書き変え
-            //issuesのtitleを取得する用
-            
-            struct Issues_List : Codable {
-                var title : String
-                var body: String
-                var number : Int
-                var comments_url : String
-            }
-
-            do{
-                let issue_List: Issues_List = try decorder.decode(Issues_List.self,from: data)
-                //issues_JSON型で
-                //print(task)
-                
-                print(issue_List.title)
-                print(issue_List.body)
-                
-                
-                query_number = issue_List.number
-            }
-            catch{
-                print(error)
-            }
-        }
     }
     
-    */
-    
-    
-    //特定のクエリを指定した時のパースを実装したもの
-    func issue(){
-        Alamofire.request(ookami_issues_url, method: .get, parameters: nil,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
-            
-            
-            
-             switch response.result {
-             case .success(let value):
-             print(value)
-             value_issue = value
-             //your success code
-             case .failure(_):
-             print("error")
-             }
-             
-            
-            
-            //guard文で書き変え
-            //パースすることに成功
-            //queryを指定した時に実施する用
-            //numberの取得
-            struct issues_JSON : Codable {
-                var title : String
-                var body: String
-                var number : Int
-                var comments_url : String
-            }
-            
-            
-            
-            guard let data=response.data else {
-                return
-            }
-            let decorder = JSONDecoder()
-            do{
-                let task : issues_JSON = try decorder.decode(issues_JSON.self,from : data)
-                //issues_JSON型で
-                //print(task)
-                print(task.title)
-                print(task.number)
-                print(task.body)
-            
-                print("クエリ設定時parseは成功!!")
-                
-                for _ in 1...7{
-                    print("")
-                }
-                
-                query_number = task.number
-                
-            }
-            catch{
-                print(error)
-            }
-        }
+    //issue_Searchを押した時のメソッド
+    @IBAction func searchIssue_Button (_ sender : UIButton){
+        textField_Number = IssueNumber_specification.text!
+        issue_number = Int(textField_Number)!
     }
+    
 }
+
 
 
 
