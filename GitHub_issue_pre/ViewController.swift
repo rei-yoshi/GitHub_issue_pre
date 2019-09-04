761c067d3f2b74956c7d77e65d681223ea1502db
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-09-04 18:28:03 +0900

    値渡しがうまくいかず、クエリの指定ができていない また、パースした内容も表示できていない

--- GitHub_issue_pre/ViewController.swift
+++ GitHub_issue_pre/ViewController.swift
@@ -8,44 +8,29 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController {
     
     @IBOutlet weak var IssueNumber_specification: UITextField!
     @IBOutlet weak var searchIssue_Button: UIButton!
-    
-   
     
     //textFieldの数値を保存用
     var textField_Number = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
     }
     
     
     //issue_Searchを押した時のメソッド
     @IBAction func searchIssue_Button (_ sender : UIButton){
+        
+        //singleissue.Swiftでも使用する
         textField_Number = IssueNumber_specification.text!
+        
         //let issue_number = Int(textField_Number)!
+        
+        //初期化
+        IssueNumber_specification.text = ""
+        
     }
-    
-   
-   
-    
 }
-
-
-
-
-
-
-
-
-
-
-
--- GitHub_issue_pre/search_issue/singleissue_ViewController.swift
+++ GitHub_issue_pre/search_issue/singleissue_ViewController.swift
@@ -12,41 +12,32 @@ import Foundation
 
 
 class singleissue_ViewController: UIViewController {
- 
     
-    @IBOutlet weak var IssueNumber_specification: UITextField!
+    
     @IBOutlet weak var searchIssue_Button: UIButton!
 
+    @IBOutlet weak var issue_title: UILabel!
+    
+    //let issues_run = Issue_number()
+    //issues_run.issue()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let issues_run = Issue_number()
         issues_run.issue()
         
-        
-        let issues_label = UILabel()
-        issues_label.text = ""
-        issues_label.sizeToFit()
-        issues_label.center = self.view.center
-        self.view.addSubview(issues_label)
-        
-        
-       
-        
+        //UIlabelにtitleとbodyを評しできるように
+         issue_title.text = "\(issues_run.title_issues)"
     }
-
-    
 }
 
+//ViewControllerのインスタンスを作成し入力値をget
+//それを用いてクエリを指定して検索をかけたい
 var temp = ViewController()
 var search_number = temp.textField_Number
 
 
-//配列型のパースを実施するためのプロトコルを定義
-public protocol Decodable {
-    init(from decoder: Decoder) throws
-}
 
 //basicm認証 成功! 2019/08/31
 //アクセス方法に自身で設定したアクセストークンを設定したことで
@@ -57,18 +48,13 @@ let password = "rei07041998"
 var value_issue:Any = 0
 var query_number : Int = 0
 
-
-
-
 //クエリを指定
 //ViewControllerクラスから入力値をもらう(issuenum.issue_number)
-let ookami_issues_url:String = "https://api.github.com/repos/ookamiinc/ios/issues/\(String(describing: search_number))"
+let ookami_issues_url:String = "https://api.github.com/repos/ookamiinc/ios/issues/\(String(search_number))"
 
+let ookami_issues_url_:String = "https://api.github.com/repos/ookamiinc/ios/issues/4909"
 
-//let ookami_issues_List_url : String = "https://api.github.com/repos/ookamiinc/ios/issues"
-
-let password1 = "7ebe192eee8f3bcf0204bbef169da832d4e1f9f2"
-
+let password1 = "6e3a2ef2616ae8390252391edb8a944b3b6b1c8a"
 let credentialData = "\(user):\(password1)".data(using: String.Encoding.utf8)!
 let base64Credentials = credentialData.base64EncodedString()
 let headers = [
@@ -78,78 +64,21 @@ let headers = [
 ]
 
 
-
-
 //issueの最新のnumberを取得する
 class Issue_number {
     
+    var title_issues :String = ""
+    var body_issues : String  = ""
+
     
-    
-    /*
-     //issuesのそれぞれのtitleを抽出して一覧表示しようと考えたが
-     //({title="",body="",...},{title="",body="",..},{title="",body="",...}のような形式の
-     //JSONデータのパースを実行できていないため一旦保留
-     //余裕があればやる
-     
-     
-     
-     //issues一覧のtitleを取得するメソッド
-     //ルートが配列の時のパースを実装する
-     func issues_List(){
-     Alamofire.request(ookami_issues_List_url, method: .get, parameters: parameters,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
-     
-     /*
-     switch response.result {
-     case .success(let value):
-     print(value)
-     value_issue = value
-     //your success code
-     case .failure(_):
-     print("error")
-     }
-     */
-     guard let data=response.data else {
-     return
-     }
-     let decorder = JSONDecoder()
-     
-     
-     //guard文で書き変え
-     //issuesのtitleを取得する用
-     
-     struct Issues_List : Codable {
-     var title : String
-     var body: String
-     var number : Int
-     var comments_url : String
-     }
-     
-     do{
-     let issue_List: Issues_List = try decorder.decode(Issues_List.self,from: data)
-     //issues_JSON型で
-     //print(task)
-     
-     print(issue_List.title)
-     print(issue_List.body)
-     
-     
-     query_number = issue_List.number
-     }
-     catch{
-     print(error)
-     }
-     }
-     }
-     
-     */
-    
+    let issue_title_label = singleissue_ViewController()
     
     //特定のクエリを指定した時のパースを実装したもの
-    func issue(){
-        Alamofire.request(ookami_issues_url, method: .get, parameters: nil,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
+    func issue() {
+        Alamofire.request(ookami_issues_url_, method: .get, parameters: nil,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
             
             
-            
+            /*
             switch response.result {
             case .success(let value):
                 print(value)
@@ -158,7 +87,7 @@ class Issue_number {
             case .failure(_):
                 print("error")
             }
-            
+            */
             
             
             //guard文で書き変え
@@ -182,6 +111,10 @@ class Issue_number {
                 let task : issues_JSON = try decorder.decode(issues_JSON.self,from : data)
                 //issues_JSON型で
                 //print(task)
+                
+                
+                
+                
                 print(task.title)
                 print(task.number)
                 print(task.body)
@@ -201,4 +134,12 @@ class Issue_number {
         }
     }
 }
+
+
+//配列型のパースを実施するためのプロトコルを定義
+public protocol Decodable {
+    init(from decoder: Decoder) throws
+}
+
+
 
