0ee00bca5d6700016cb67189c5995fa307ef9750
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-09-04 14:37:33 +0900

    UIlabelの追加

--- GitHub_issue_pre/search_issue/singleissue_ViewController.swift
+++ GitHub_issue_pre/search_issue/singleissue_ViewController.swift
@@ -5,26 +5,211 @@
 //  Created by 吉田れい on 2019/09/04.
 //  Copyright © 2019 吉田れい. All rights reserved.
 //
+
 import UIKit
+import Alamofire
+import Foundation
+
 
 class singleissue_ViewController: UIViewController {
+ 
     
+    @IBOutlet weak var IssueNumber_specification: UITextField!
+    @IBOutlet weak var searchIssue_Button: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let issues_label = UILabel()
+        issues_label.text = ""
+            issues_label.sizeToFit()
+        issues_label.center = self.view.center
+        self.view.addSubview(issues_label)
         
-        // Do any additional setup after loading the view.
+        
+       
+        
     }
+    
+    func run_issues(){
+        let issues_run = Issue_number()
+        issues_run.issue()
+    }
+   
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+//配列型のパースを実施するためのプロトコルを定義
+public protocol Decodable {
+    init(from decoder: Decoder) throws
+}
+
+//basicm認証 成功! 2019/08/31
+//アクセス方法に自身で設定したアクセストークンを設定したことで
+//ookami_organizationへのアクセス権限を得ることができた
+
+let user = "rei-yoshi"
+let password = "rei07041998"
+var value_issue:Any = 0
+var query_number : Int = 0
+
+let issuenum = ViewController()
+
+
+
+
+//クエリを指定
+//ViewControllerクラスから入力値をもらう(issuenum.issue_number)
+let ookami_issues_url:String = "https://api.github.com/repos/ookamiinc/ios/issues/\(issuenum.issue_number)"
+
+
+//let ookami_issues_List_url : String = "https://api.github.com/repos/ookamiinc/ios/issues"
+
+//let ookami_issues_comments_url = "https://api.github.com/repos/ookamiinc/ios/issues/4862"
+
+
+let password1 = "7ebe192eee8f3bcf0204bbef169da832d4e1f9f2"
+let credentialData = "\(user):\(password1)".data(using: String.Encoding.utf8)!
+let base64Credentials = credentialData.base64EncodedString()
+let headers = [
+    "Authorization": "Basic \(base64Credentials)",
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+]
+
+
+
+
+//issueの最新のnumberを取得する
+class Issue_number {
+    
     
     
     /*
-     // MARK: - Navigation
+     //issuesのそれぞれのtitleを抽出して一覧表示しようと考えたが
+     //({title="",body="",...},{title="",body="",..},{title="",body="",...}のような形式の
+     //JSONデータのパースを実行できていないため一旦保留
+     //余裕があればやる
      
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
+     
+     
+     //issues一覧のtitleを取得するメソッド
+     //ルートが配列の時のパースを実装する
+     func issues_List(){
+     Alamofire.request(ookami_issues_List_url, method: .get, parameters: parameters,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
+     
+     /*
+     switch response.result {
+     case .success(let value):
+     print(value)
+     value_issue = value
+     //your success code
+     case .failure(_):
+     print("error")
      }
      */
+     guard let data=response.data else {
+     return
+     }
+     let decorder = JSONDecoder()
+     
+     
+     //guard文で書き変え
+     //issuesのtitleを取得する用
+     
+     struct Issues_List : Codable {
+     var title : String
+     var body: String
+     var number : Int
+     var comments_url : String
+     }
+     
+     do{
+     let issue_List: Issues_List = try decorder.decode(Issues_List.self,from: data)
+     //issues_JSON型で
+     //print(task)
+     
+     print(issue_List.title)
+     print(issue_List.body)
+     
+     
+     query_number = issue_List.number
+     }
+     catch{
+     print(error)
+     }
+     }
+     }
+     
+     */
     
+    
+    //特定のクエリを指定した時のパースを実装したもの
+    func issue(){
+        Alamofire.request(ookami_issues_url, method: .get, parameters: nil,encoding: URLEncoding(destination: .queryString), headers: headers) .responseJSON { response in
+            
+            
+            
+            switch response.result {
+            case .success(let value):
+                print(value)
+                value_issue = value
+            //your success code
+            case .failure(_):
+                print("error")
+            }
+            
+            
+            
+            //guard文で書き変え
+            //パースすることに成功
+            //queryを指定した時に実施する用
+            //numberの取得
+            struct issues_JSON : Codable {
+                var title : String
+                var body: String
+                var number : Int
+                var comments_url : String
+            }
+            
+            
+            
+            guard let data=response.data else {
+                return
+            }
+            let decorder = JSONDecoder()
+            do{
+                let task : issues_JSON = try decorder.decode(issues_JSON.self,from : data)
+                //issues_JSON型で
+                //print(task)
+                print(task.title)
+                print(task.number)
+                print(task.body)
+                
+                print("クエリ設定時parseは成功!!")
+                
+                for _ in 1...7{
+                    print("")
+                }
+                
+                query_number = task.number
+                
+            }
+            catch{
+                print(error)
+            }
+        }
+    }
 }
+
